a3eb305313ae5d806d697ae043bb4b40310cb89c
Gaston <gaston@pitagorasc.com.ar>
2021-08-17 11:24:20 -0300

    share url

--- script.js
+++ script.js
@@ -23,11 +23,18 @@ headers: {
         title.innerText = `${data.Article}`;
         Leer.setAttribute("href", data.Source);
         const Shared = encodeURIComponent(data.Twitter.trim());
-        const tuit = 'https://twitter.com/intent/tweet?text=' + Shared;
+        const urlQuote = encodeURIComponent(data.Quote.trim());
+        const urlArticle = encodeURIComponent(data.Article.trim());
+        const tuit = 'https://twitter.com/intent/tweet?text="' + urlQuote + '"%0AExtraído%20de%20"' + urlArticle + '"%20por%20@leopiccioli%20https://bit.ly/3jYFTkY';
         document.getElementById("Shared").setAttribute("href", tuit);
     } catch (error) {
         console.log('Fetch Error', error);
     };
 };
-
+//https://twitter.com/intent/tweet?text=
+//quote = %22No%20compitas%20si%20sab%C3%A9s%20que%20vas%20a%20perder%20y%20no%20vas%20a%20aprender%22%20%20%0D
+//Extraído%20de%20
+//%20%22%C2%BF
+//article = Hablo%20Yo%20o%20Pasa%20un%20Tren?%20S02E07%22%20
+//por%20@leopiccioli%20http://j.mp/leocita
 getData();
